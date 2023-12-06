d71bbd7353d0b8a1f42147cc28d11e61fb7e4493
Qiro <valdecanasjustin@gmail.com>
2023-12-07 03:33:12 +0800

    Add project files.

---
+++ FinalsELEC/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using FinalsELEC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalsELEC.Data
+{
+    public class AppDbContext : DbContext
+    {
+    }
+}
---
+++ FinalsELEC/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinalsELEC.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryID { get; set; }
+        public int StockQuantity { get; set; }
+        public int BrandID { get; set; }
+    }
+}
